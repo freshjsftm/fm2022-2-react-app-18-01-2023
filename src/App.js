fdfb14c95a07a3c0ca4b4e24631dda0fb429ce0e
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-19 09:42:36 +0200

    Add map users

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import {Component} from "react";
+import { Component } from "react";
 import "./App.css";
 import Ciao from "./components/Ciao";
 
-class App extends Component{
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users:[
+      users: [
         {
-          id:1,
+          id: 1,
           firstName: "Brad",
           lastName: "Pitt",
         },
         {
-          id:2,
+          id: 2,
           firstName: "Tom",
           lastName: "Rot",
         },
         {
-          id:3,
+          id: 3,
           firstName: "Jim",
           lastName: "Carry",
         },
         {
-          id:4,
+          id: 4,
           firstName: "Rob",
           lastName: "Qwerty",
         },
         {
-          id:5,
+          id: 5,
           firstName: "Leo",
           lastName: "Kaprio",
         },
-      ]
+      ],
     };
   }
   render() {
-    const {users} = this.state;
-    return (
-      <>
-        <h1 className="heading" title="JSX">
-          Hi, JSX!
-        </h1>
-        <Ciao username={`${users[0].firstName} ${users[0].lastName}`}/>
-      </>
-    );
+    const { users } = this.state;
+    const liArray = users.map(({ firstName, lastName }, index) => (
+      <li key={index}>
+        <Ciao username={`${firstName} ${lastName}`} />
+      </li>
+    ));
+    return <ul>{liArray}</ul>;
   }
 }
 
 export default App;
+
+// users.map((user)=>component)
