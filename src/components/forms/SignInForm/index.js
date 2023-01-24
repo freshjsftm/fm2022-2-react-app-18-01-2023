ad1d41ae3e269bfa536e7cadda0790f2b834c011
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-24 10:28:44 +0200

    Add valid/invalid

--- src/components/forms/SignInForm/index.js
+++ src/components/forms/SignInForm/index.js
@@ -1,22 +1,53 @@
-import React, { Component } from 'react';
-import styles from './SignInForm.module.css';
+import React, { Component } from "react";
+import styles from "./SignInForm.module.css";
 
+const initialValues = {
+  email: "",
+  password: "",
+  isemailValid: false,
+  ispasswordValid: false,
+};
 class SignInForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = { ...initialValues };
   }
-  formHandler = (event)=>{
+  formHandler = (event) => {
     event.preventDefault();
-    console.log(event)
-  }
-  
+    event.target.reset();
+    this.setState({ ...initialValues });
+  };
+  // emailHandler = ({target:{value}}) => this.setState({email:value})
+  // passwordHandler = ({target:{value}}) => this.setState({password:value})
+  inputHandler = ({ target: { name, value } }) =>
+    this.setState({ [name]: value, [`is${name}Valid`]: !value.includes(" ") });
   render() {
+    const { email, password, isemailValid, ispasswordValid } = this.state;
+    const emailClasses = `${styles.input} ${
+      isemailValid ? styles.valid : styles.invalid
+    }`;
+    const passClasses = `${styles.input} ${
+      ispasswordValid ? styles.valid : styles.invalid
+    }`;
     return (
       <form className={styles.form} onSubmit={this.formHandler}>
-        <input type='email' name='email' placeholder='email'/>
-        <input type='password' name='password' placeholder='password'/>
-        <input type='submit' value='send'/>
+        <input
+          value={email}
+          onChange={this.inputHandler}
+          className={emailClasses}
+          type="email"
+          name="email"
+          placeholder="email"
+        />
+        <input
+          value={password}
+          onChange={this.inputHandler}
+          className={passClasses}
+          type="password"
+          name="password"
+          placeholder="password"
+        />
+        <input type="submit" value="send" />
       </form>
     );
   }
