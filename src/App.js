7eef07c83022e298f75f53d1b73129235ca1b46d
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-30 09:38:49 +0200

    Add example prop children

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from "react";
 import "./App.css";
-import StopWatch from "./components/StopWatch";
 
-function App() {
+const List = (props) => {
+  const { title, children } = props;
+  return (
+    <article title={title}>
+      <h2>List</h2>
+      {children}
+    </article>
+  );
+};
+
+function App(props) {
   return (
     <>
-      <StopWatch />
+    <List title="list">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </List>
+    <List>
+      <ol>
+        <li>qwerty</li>
+        <li>qwerty</li>
+        <li>qwerty</li>
+      </ol>
+    </List>
     </>
   );
+  // const list = React.createElement(
+  //   "ul",
+  //   {title:'list'},
+  //   React.createElement("li", {}, "1"),
+  //   React.createElement("li", {}, "2"),
+  //   React.createElement("li", {}, "3")
+  // );
+  // return list;
 }
 
 export default App;
