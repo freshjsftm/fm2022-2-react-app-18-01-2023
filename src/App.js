7e9f0ce7f6c9c9d159eea584f0bce5c41502772a
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-03 08:48:33 +0200

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React from "react";
-import WindowSize from "./components/WindowSize";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import WindowSize from './components/WindowSize';
+import SignInForm from './components/forms/SignInForm';
+import StopWatch from './components/StopWatch';
+import Error404 from './components/Error404';
 
 function App(props) {
   return (
     <>
-      <WindowSize />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><NavLink to="/">HOME</NavLink></li>
+            <li><NavLink to="/sign-in">sign-in</NavLink></li>
+            <li><NavLink to="/stop-watch">StopWatch</NavLink></li>
+            <li><NavLink to="/dash-board">Dashboard</NavLink>
+              <ul>
+                <li><NavLink to="/dash-board/messages">messages</NavLink></li>
+                <li><NavLink to="/dash-board/tasks">tasks</NavLink></li>
+                <li><NavLink to="/dash-board/users">users</NavLink></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<WindowSize />} />
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/stop-watch" element={<StopWatch />} />
+          <Route path='/dash-board/' element={<Dashboard />}>
+            <Route path="messages" element={<h2>messages</h2>} />
+            <Route path="tasks" element={<h2>tasks</h2>} />
+            <Route path="users" element={<h2>users</h2>} />
+          </Route>
+
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+
+        <footer>
+          <nav>
+            <ul>
+              <li><NavLink to="/">HOME</NavLink></li>
+              <li><NavLink to="/sign-in">sign-in</NavLink></li>
+              <li><NavLink to="/stop-watch">StopWatch</NavLink></li>
+              <li><NavLink to="/dash-board">Dashboard</NavLink>
+                <ul>
+                  <li><NavLink to="/dash-board/messages">messages</NavLink></li>
+                  <li><NavLink to="/dash-board/tasks">tasks</NavLink></li>
+                  <li><NavLink to="/dash-board/users">users</NavLink></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+          <p>&copy; 2023</p>
+        </footer>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Error404/index.js
+++ src/components/Error404/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import ImageWrapper from '../ImageWrapper';
 
 const Error404 = () => {
+  const navigate = useNavigate();
+  setTimeout(()=>{navigate('/', {replace:true})},2000);
   return (
     <ImageWrapper width='100%'>
       <img src="images/404-error-1560x760.png" alt="error 404"/>
-    </ImageWrapper>
+    </ImageWrapper>   
   );
 }
 
