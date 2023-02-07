2617cf6015b21f791fa71c2a95315864b6203b4c
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-07 09:59:51 +0200

    Add HOC WithTheme

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,57 @@
 import React, { Component } from "react";
-import {WbSunny, DarkMode} from '@mui/icons-material';
+import { WbSunny, DarkMode } from "@mui/icons-material";
 import cx from "classnames";
 import { UserContext, ThemeContext } from "../../contexts";
+import { WithTheme } from "../HOC";
 import styles from "./Header.module.scss";
 import CONSTANTS from "../../constants";
 const { THEMES } = CONSTANTS;
 
 class Header extends Component {
   render() {
+    const { theme, setTheme } = this.props;
+    const classNames = cx(styles.header, {
+      [styles.light]: theme === THEMES.LIGHT,
+      [styles.dark]: theme === THEMES.DARK,
+    });
     return (
-      <ThemeContext.Consumer>
-        {([theme, setTheme]) => {
-          const classNames = cx(styles.header,
-            {
-            [styles.light]: theme === THEMES.LIGHT,
-            [styles.dark]: theme === THEMES.DARK,
-          });
-          return (
-            <UserContext.Consumer>
-              {(user) => (
-                <header className={classNames}>
-                  <strong>hi,{user.firstName}</strong>
-                  <span className={styles.theme}
-                    onClick={() => {
-                      const newTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
-                      setTheme(newTheme);
-                    }}
-                  >
-                    {theme === THEMES.LIGHT ? <DarkMode /> : <WbSunny />}
-                  </span>
-                </header>
-              )}
-            </UserContext.Consumer>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <UserContext.Consumer>
+        {(user) => (
+          <header className={classNames}>
+            <strong>hi,{user.firstName}</strong>
+            <span
+              className={styles.theme}
+              onClick={() => {
+                const newTheme =
+                  theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
+                setTheme(newTheme);
+              }}
+            >
+              {theme === THEMES.LIGHT ? <DarkMode /> : <WbSunny />}
+            </span>
+          </header>
+        )}
+      </UserContext.Consumer>
     );
   }
 }
+export default WithTheme(Header);
 
-export default Header;
+// const HeaderWithContext = () => {
+//   return (
+//     <ThemeContext.Consumer>
+//       {([theme, setTheme]) => <Header theme={theme} setTheme={setTheme} />}
+//     </ThemeContext.Consumer>
+//   );
+// };
+// const WithTheme = (InnerComponent) => (props) => {
+//   return (
+//     <ThemeContext.Consumer>
+//       {([theme, setTheme]) => (
+//         <InnerComponent theme={theme} setTheme={setTheme} />
+//       )}
+//     </ThemeContext.Consumer>
+//   );
+// };
+//const HeaderWithContext = WithTheme(Header);
+
--- src/components/Parent/index.js
+++ src/components/Parent/index.js
@@ -2,27 +2,41 @@ import React from "react";
 import cx from "classnames";
 import UserCard from "../UserSection/UserCard";
 import { UserContext, ThemeContext } from "../../contexts";
+import { WithTheme } from "../HOC";
 import styles from "./Parent.module.scss";
 import CONSTANTS from "../../constants";
 const { THEMES } = CONSTANTS;
 
-const Parent = () => {
-  const renderUserContext = (user) => <UserCard user={user} />;
+const Parent = (props) => {
+  const { theme } = props;
+  const classNames = cx({
+    [styles.light]: theme === THEMES.LIGHT,
+    [styles.dark]: theme === THEMES.DARK,
+  });
   return (
-    <ThemeContext.Consumer>
-      {([theme]) => {
-        const classNames = cx({
-          [styles.light]: theme === THEMES.LIGHT,
-          [styles.dark]: theme === THEMES.DARK,
-        });
-        return (
-          <section className={classNames}>
-            <h1>Parent</h1>
-            <UserContext.Consumer>{renderUserContext}</UserContext.Consumer>
-          </section>
-        );
-      }}
-    </ThemeContext.Consumer>
+    <section className={classNames}>
+      <h1>Parent</h1>
+      <UserContext.Consumer>
+        {(user) => <UserCard user={user} />}
+      </UserContext.Consumer>
+    </section>
   );
 };
-export default Parent;
+export default WithTheme(Parent);
+
+// const ParentWithContext = (props) => {
+//   return (
+//     <ThemeContext.Consumer>
+//       {([theme, setTheme]) => <Parent theme={theme} setTheme={setTheme} />}
+//     </ThemeContext.Consumer>
+//   );
+// };
+// const WithTheme = (InnerComponent) => (props)=>{
+//   return (
+//     <ThemeContext.Consumer>
+//       {([theme, setTheme]) => <InnerComponent theme={theme} setTheme={setTheme} />}
+//     </ThemeContext.Consumer>
+//   );
+// }
+//const ParentWithContext = WithTheme(Parent);
+
