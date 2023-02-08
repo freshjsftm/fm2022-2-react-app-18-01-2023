6ebb6f3ec9c9530d50aaad571f78045fea8915c4
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-08 10:26:35 +0200

    Add Formik

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import LogInForm from "./components/forms/LogInForm";
-import { LOG_IN_SCHEMA } from "./utils/validationSchemas";
 
 function App() {
-  const userDataLogIn = {
-    login: "qwerty",
-    password: "gr3at@3wdsG",
-  };
-  console.log(
-    LOG_IN_SCHEMA.validate(userDataLogIn)
-      .then((values) => console.log(values))
-      .catch((err)=> console.log(err))
-  );
+
   return (
     <>
-      <LogInForm />
+      <LogInForm/>
     </>
   );
 }
--- src/components/forms/LogInForm/index.js
+++ src/components/forms/LogInForm/index.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
-
-class LogInForm extends Component {
-  render() {
-    return (
-      <form>
-        <input name="login"/>
-        <input name="password"/>
-      </form>
-    );
-  }
-}
-
+import React from "react";
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import { LOG_IN_SCHEMA } from "../../../utils/validationSchemas";
+const initialValues = {
+  login: "",
+  password: "",
+};
+const LogInForm = (props) => {
+  const onSubmit = (values, formikBag) => {
+    console.group();
+    console.log(values);
+    console.log(formikBag);
+    console.groupEnd();
+    formikBag.resetForm();
+  };
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={LOG_IN_SCHEMA}
+    >
+      {(formikProps) => {
+        //console.log(formikProps);
+        return (
+          <Form>
+            <Field name="login" placeholder="login" />
+            <ErrorMessage name="login" component="div" />
+            <Field name="password" type="password" placeholder="password" />
+            <ErrorMessage name="password" component="div" />
+            <Field type="submit" value="SEND" />
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
 export default LogInForm;
