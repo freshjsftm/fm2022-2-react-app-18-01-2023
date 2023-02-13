6c49fc225271c77bd3314bbd67366c09210166d2
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-13 10:39:24 +0200

    Add good practice initial state

--- src/components/LearnHooks/index.js
+++ src/components/LearnHooks/index.js
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
 
 const LearnHooks = (props) => {
-  const [state, setState] = useState({
+  const [coords, setCoords] = useState({
     x: 0,
-    y: 0,
-    count:0
+    y: 0
   });
+  const [count, setCount] = useState(0);
   const handlerMove = (event) => {
-    setState({
-      ...state,
+    setCoords({
       x: event.clientX,
       y: event.clientY,
     });
   };
-  const handlerClick = ()=>{setState({...state,count:state.count+1})}
+  const handlerClick = ()=>{setCount((prevCount)=>prevCount+1)}
   return (
     <div style={{ height: "95vh" }} onMouseMove={handlerMove} onClick={handlerClick}>
-      <p>x: {state.x}</p>
-      <p>y: {state.y}</p>
-      <p>count: {state.count}</p>
+      <p>x: {coords.x}</p>
+      <p>y: {coords.y}</p>
+      <p>count: {count}</p>
     </div>
   );
 };
-
 export default LearnHooks;
