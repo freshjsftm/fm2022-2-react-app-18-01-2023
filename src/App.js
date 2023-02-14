bc51bc8030f1fbf6ffcf8dca4d64cb25b3446635
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-14 12:10:43 +0200

    Add FuncHeader

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FuncHeader from "./components/FuncHeader";
 import UserProfile from "./components/UserProfile";
-import { UserContext } from "./contexts";
-
+import { UserContext, ThemeContext } from "./contexts";
+import CONSTANTS from "./constants";
+const { THEMES } = CONSTANTS;
 function App() {
   const [user, setUser] = useState({
     id: 1,
     name: "Brad Pitt",
   });
+  const [theme, setTheme] = useState(THEMES.LIGHT);
   return (
-    <UserContext.Provider value={[user, setUser]}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<UserProfile />} />
-        </Routes>
-      </BrowserRouter>
-    </UserContext.Provider>
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <UserContext.Provider value={[user, setUser]}>
+        <BrowserRouter>
+          <FuncHeader />
+          <Routes>
+            <Route path="/" element={<UserProfile />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 export default App;
---
+++ src/components/FuncHeader/index.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { WbSunny, DarkMode } from "@mui/icons-material";
+import cx from "classnames";
+import { UserContext, ThemeContext } from "../../contexts";
+import styles from "./FuncHeader.module.scss";
+import CONSTANTS from "../../constants";
+const { THEMES } = CONSTANTS;
+
+const FuncHeader = () => {
+  const [user, setUser] = useContext(UserContext);
+  const [theme, setTheme] = useContext(ThemeContext);
+  const handlerTheme = () => {
+    const newTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
+    setTheme(newTheme);
+  };
+  const classNames = cx(styles.header, {
+    [styles.light]: theme === THEMES.LIGHT,
+    [styles.dark]: theme === THEMES.DARK,
+  });
+  return (
+    <header className={classNames}>
+      <strong>hi,{user.name}</strong>
+      <span onClick={handlerTheme}>
+        {theme === THEMES.LIGHT ? <DarkMode /> : <WbSunny />}
+      </span>
+    </header>
+  );
+};
+
+export default FuncHeader;
