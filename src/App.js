dffa662782f215f0f4bca1c85dff1128e96b3778
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-20 09:35:48 +0200

    Prepare for todo practice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useState, useReducer, createRef, useEffect } from "react";
-import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import React, { useState, useReducer } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MenuOpen } from "@mui/icons-material";
 import { UserContext, ThemeContext, MenuContext } from "./contexts";
 import FuncHeader from "./components/FuncHeader";
 import SignUpForm from "./components/forms/SignUpForm";
 import Chat from "./components/Chat";
 import NavMenu from "./components/NavMenu";
+import TodoReducer from "./components/TodoReducer";
+import TodoUseState from "./components/TodoUseState";
 import HomePage from "./pages/HomePage";
 import { useClicker } from "./hooks";
 import CONSTANTS from "./constants";
+
 const { THEMES } = CONSTANTS;
 const reducer = (state, action) => {
   switch (action.type) {
@@ -41,18 +44,6 @@ function App() {
     return dispatch({ type: "MENU_OPEN" });
   };
   const closeMenu = () => dispatch({ type: "MENU_CLOSE" });
-    
-  const navMenuRef = createRef();
-  useEffect(() => {
-    const handlerClick = ({ target }) => {
-      if (navMenuRef.current.contains(target) === false && state.isMenuOpen) {
-        closeMenu();
-      }
-    };
-    window.addEventListener("click", handlerClick);
-    return () => window.removeEventListener("click", handlerClick);
-    // eslint-disable-next-line
-  }, [state.isMenuOpen]);
 
   return (
     <MenuContext.Provider value={[state, closeMenu]}>
@@ -60,26 +51,15 @@ function App() {
         <UserContext.Provider value={[user, setUser]}>
           <BrowserRouter>
             <MenuOpen onClick={openMenu} />
-            <NavMenu navMenuRef={navMenuRef}/>
+            <NavMenu/>
             <FuncHeader />
             <h2>Count click at window = {count}</h2>
-            <nav>
-              <ul>
-                <li>
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/signup">signup</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/chat">chat</NavLink>
-                </li>
-              </ul>
-            </nav>
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/signup" element={<SignUpForm />} />
               <Route path="/chat" element={<Chat />} />
+              <Route path="/todo-1" element={<TodoReducer />} />
+              <Route path="/todo-2" element={<TodoUseState />} />
             </Routes>
           </BrowserRouter>
         </UserContext.Provider>
--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -10,20 +10,30 @@ const NavMenu = (props) => {
   const classNames = cx(styles.container, {
     [styles.open]: state.isMenuOpen,
   });
-  //const navMenuRef = useRef(null);
-  // useEffect(() => {
-  //   const handlerClick = ({ target }) => {
-  //     if (navMenuRef.current.contains(target) === false && state.isMenuOpen) {
-  //       closeMenu();
-  //     }
-  //   };
-  //   window.addEventListener("click", handlerClick);
-  //   return () => window.removeEventListener("click", handlerClick);
-  //   // eslint-disable-next-line
-  // }, [state.isMenuOpen]);
+  const navMenuRef = useRef(null);
+  useEffect(() => {
+    const handlerClick = ({ target }) => {
+      if (navMenuRef.current.contains(target) === false && state.isMenuOpen) {
+        closeMenu();
+      }
+    };
+    window.addEventListener("click", handlerClick);
+    return () => window.removeEventListener("click", handlerClick);
+    // eslint-disable-next-line
+  }, [state.isMenuOpen]);
+  const keyCloseMenu = (event) => {
+    if (event.keyCode === 13) {
+      closeMenu();
+    }
+  };
   return (
-    <nav className={classNames} ref={props.navMenuRef}>
-      <CancelPresentation className={styles.closeBtn} onClick={closeMenu} />
+    <nav className={classNames} ref={navMenuRef}>
+      <CancelPresentation
+        className={styles.closeBtn}
+        onClick={closeMenu}
+        tabIndex="0"
+        onKeyDown={keyCloseMenu}
+      />
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -33,6 +43,12 @@ const NavMenu = (props) => {
         </li>
         <li>
           <NavLink to="/chat">chat</NavLink>
+        </li>
+        <li>
+          <NavLink to="/todo-1">todo reducer</NavLink>
+        </li>
+        <li>
+          <NavLink to="/todo-2">todo useState</NavLink>
         </li>
       </ul>
     </nav>
